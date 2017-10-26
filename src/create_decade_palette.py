5a7ef992aeffc578d7bf5311c2fae0812afcf16b
TaylorDiego <Hagin.Taylor@gmail.com>
2017-10-26 16:16:29 -0700

    add decade_list to create_decade_palette

--- src/create_decade_palette.py
+++ src/create_decade_palette.py
@@ -5,9 +5,15 @@ import pandas as pd
 with open('../../pickle/fashion.pkl', 'rb') as f:
     df = pickle.load(f)
 
+print(df.head())
+
 decade_list = [decade for decade in sorted(df['decade'].unique())]
 
-print(decade_list)
+decade_palettes = {}
 
 def create_decade_palette(df):
-    pass
+    for decade in decade_list:
+        decade_palettes[decade] = list(df['palette'])
+    return decade_palettes
+
+create_decade_palette(df)
