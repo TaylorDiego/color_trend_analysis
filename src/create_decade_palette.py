4b0899aa7fe630138124fed563dee22a2a6d73dc
TaylorDiego <Hagin.Taylor@gmail.com>
2017-10-31 16:40:41 -0700

    with new pickle in create_decade_palette.py

--- src/create_decade_palette.py
+++ src/create_decade_palette.py
@@ -2,7 +2,7 @@ from set_similarity import SetSimilarity
 import pickle
 import pandas as pd
 
-with open('../../pickle/fashion.pkl', 'rb') as f:
+with open('../../pickle/sliced_fashion.pkl', 'rb') as f:
     df = pickle.load(f)
 
 # print(df.head())
